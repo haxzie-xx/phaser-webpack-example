b36a0a63c737d00fb5d9fcaf15d2ec2937efaee8
Musthaq Ahamad <musthu.gm@gmail.com>
2018-10-21 12:28:32 +0530

    ADD: Webpack hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,79 @@
-const path = require('path');
+var path = require('path')
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+// Phaser webpack config
+var phaserModule = path.join(__dirname, '/node_modules/phaser/')
+var phaser = path.join(phaserModule, 'src/phaser.js')
+
+var definePlugin = new webpack.DefinePlugin({
+    __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
+    WEBGL_RENDERER: true, // I did this to make webpack work, but I'm not really sure it should always be true
+    CANVAS_RENDERER: true // I did this to make webpack work, but I'm not really sure it should always be true
+})
 
 module.exports = {
-    mode: 'production',
     entry: {
-        //entry point of the game
-        app: path.resolve(__dirname, 'src/index.js'),
+        app: [
+            path.resolve(__dirname, 'src/index.js')
+        ],
+        vendor: ['phaser']
     },
+    devtool: 'cheap-source-map',
     output: {
+      pathinfo: true,
         path: path.resolve(__dirname, 'dist'),
-        filename: 'game.bundle.js'
+        publicPath: './dist/',
+        library: '[name]',
+        libraryTarget: 'umd',
+        filename: '[name].js'
     },
-    module: {
-        rules: [{
-            test: /\.js?$/,
-            include: [ path.resolve(__dirname, "src")],
-            exclude: [ path.resolve(__dirname, "node_modules")],
-            loader: 'babel-loader',
-            query: {
-                presets: ["@babel/env"]
+    watch: true, 
+    plugins: [
+        definePlugin,
+        //new webpack.optimize.CommonsChunkPlugin({ name: 'vendor'/* chunkName= */, filename: 'vendor.bundle.js'/* filename= */ }),
+        new HtmlWebpackPlugin({
+            filename: '../index.html',
+            template: './src/index.html',
+            chunks: ['vendor', 'app'],
+            chunksSortMode: 'manual',
+            minify: {
+                removeAttributeQuotes: false,
+                collapseWhitespace: false,
+                html5: false,
+                minifyCSS: false,
+                minifyJS: false,
+                minifyURLs: false,
+                removeComments: false,
+                removeEmptyAttributes: false
+            },
+            hash: false
+        }),
+        new BrowserSyncPlugin({
+            host: process.env.IP || 'localhost',
+            port: process.env.PORT || 3000,
+            server: {
+                baseDir: ['./', './dist']
             }
-        }]
+        })
+    ],
+    module: {
+        rules: [
+            { test: /\.js$/, use: ['babel-loader'], include: path.join(__dirname, 'src') },
+            { test: /phaser-split\.js$/, use: ['expose-loader?Phaser'] },
+            { test: [/\.vert$/, /\.frag$/], use: 'raw-loader' }
+        ]
     },
+   /* node: {
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty'
+    },
+    resolve: {
+        alias: {
+            'phaser': phaser,
+        }
+    }*/
 }
